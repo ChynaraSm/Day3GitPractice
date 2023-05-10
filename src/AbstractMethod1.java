476161a74de1e811c77afe48f23890cd8621697b
ChynaraSm <ckemelzhong@yahoo.com>
2023-05-10 23:05:45 +0800

    added more about Abstract class

--- src/AbstractMethod1.java
+++ src/AbstractMethod1.java
@@ -1,5 +1,9 @@
 public class AbstractMethod1 {
 
-    //Abstract Method can only be created in abstract class
+    //Abstract Method can only be created in abstract class or interface
+
+    //Abstract class is designed to be a parent class
+
+    //The abstract keyword is used to create the abstract class
 
 }
