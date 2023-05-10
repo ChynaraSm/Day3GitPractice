dd39b67ba42fd294aeb45c515b1178e1e71d20af
ChynaraSm <ckemelzhong@yahoo.com>
2023-05-10 22:48:51 +0800

    added information to Abstraction intro class

--- src/AbstractionIntro.java
+++ src/AbstractionIntro.java
@@ -3,4 +3,8 @@ public class AbstractionIntro {
     //abstraction focuses on relevant properties
 
     //abstract methods don't have method body
+
+    //Abstraction is process of hiding implementation details from the user
+
+    //only the functionality will be provided to the user
 }
